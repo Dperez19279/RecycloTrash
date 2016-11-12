76761ce2639ae397109dbd78a18902e64b6461c4
David Perez <dperez19279@columbushs.com>
2016-11-12 09:50:50 -0500

    Complete dsreader integration

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python
 
 import argparse
+from time import sleep
+import subprocess
+import timeout
+import atexit
+
+def killall():
+    subprocess.call("killall dsreader",shell=True)
+
+atexit.register(killall)
 
 parser = argparse.ArgumentParser(description="Control the RecycloTrash")
 
@@ -13,7 +22,7 @@ group_servo = parser.add_mutually_exclusive_group(required=True)
 
 group_motion.add_argument('--NoMotion', action='store_true', help="Skip motion detection")
 group_motion.add_argument('--camera', action='store_true', help="Use camera to detect motion")
-group_motion.add_argument('--pir', action='store_true', help="Use infrared motion detector")
+group_motion.add_argument('--pir', type=int, help="Use infrared motion detector")
 group_qr.add_argument('--NoneT', action='store_true', help="Assume trash")
 group_qr.add_argument('--NoneR', action='store_true', help="Assume recycle")
 group_qr.add_argument('--NoneRand', action='store_true', help="Randomize between trash and recycle")
@@ -32,6 +41,8 @@ gpio_imported=False
 motion_pin=None
 gpio_pin=True
 
+camera_in_use=0
+
 def verbose_print(str):
     if args.verbose:
         print(str)
@@ -42,8 +53,9 @@ if args.NoMotion:
     verbose_print("Motion: No Motion")
 elif args.camera:
     verbose_print("Motion: Use PiCamera")
+    camera_in_use+=1
 elif args.pir:
-    verbose_print("Motion: Use PIR Motion Detector, PIN: "+args.pir)
+    verbose_print("Motion: Use PIR Motion Detector, PIN: "+str(args.pir))
     import RPi.GPIO as GPIO
     GPIO.setmode(GPIO.BOARD)
     motion_pin=args.pir
@@ -60,8 +72,10 @@ elif args.NoneRand:
     verbose_print("Object Identification: Randomly pick")
 elif args.dsreader:
     verbose_print("Object Identification: Use dsreader with video0")
+    camera_in_use+=1
 elif args.image:
     verbose_print("Object Identification: Take photo to find QR/Barcode")
+    camera_in_use+=1
 else:
     raise ValueError("No Object Identification Value")
 
@@ -69,13 +83,14 @@ if args.NoCV:
     verbose_print("Computer Vision: None")
 elif args.googlevision:
     verbose_print("Computer Vision: Use Google Vision")
+    camera_in_use+=1
 else:
     raise ValueError("No Object Identification Value")
 
 if args.NoServo:
     verbose_print("Trash movement: None")
 elif args.gpio:
-    verbose_print("Trash movement: Use Servo, PIN: " + args.gpio)
+    verbose_print("Trash movement: Use Servo, PIN: " + str(args.gpio))
     if not gpio_imported:
         import RPi.GPIO as GPIO
         GPIO.setmode(GPIO.BOARD)
@@ -84,3 +99,42 @@ elif args.gpio:
     gpio_imported=True
 else:
     raise ValueError("No Trash Movement Value")
+
+#Post-Init
+
+if not camera_in_use>1:
+    from subprocess import Popen, PIPE
+    from fcntl import fcntl, F_GETFL, F_SETFL
+    from os import O_NONBLOCK, read
+    p = Popen(['/home/pi/RecycloTrash/QR/dsreader'], stdin = PIPE, stdout = PIPE, stderr = PIPE, shell = False, cwd="/home/pi/RecycloTrash/QR/")
+    flags = fcntl(p.stdout, F_GETFL)
+    fcntl(p.stdout, F_SETFL, flags | O_NONBLOCK)    
+    
+while 1:
+    if args.pir:
+        verbose_print("Motion: Start PIR")
+#        while GPIO.input(motion_pin):            sleep(0.3)
+        verbose_print("Motion: Looking for new motion")
+        while not GPIO.input(motion_pin):
+            sleep(0.3)
+        verbose_print("Motion: Motion found")
+    if args.camera:
+        verbose_print("Motion: Start PiCamera")
+        subprocess.check_output(["/usr/bin/python","/home/pi/RecycloTrash/motion.py"])
+        verbose_print("Motion: Motion found")
+    if args.dsreader:
+        if camera_in_use>1:
+            raise ValueError("Not Implemented")
+        if not camera_in_use>1:
+            try:
+                verbose_print("Datasymbol: Begin search")
+                sleep(10)
+                raw_dsreader_input=read(p.stdout.fileno(), 1024)
+                verbose_print(raw_dsreader_input)
+            except OSError:
+                raw_dsreader_input=""
+                verbose_print("Datasymbol: No datasymbol")
+            #qr=raw_dsreader_input.split
+    sleep(3)
+    read(p.stdout.fileno(), 1024)
+    print("="*20)
